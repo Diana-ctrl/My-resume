dcbe8e2e97133081d9e8cd776cf52c8ed8112c82
Diana Lyah <d.liakh@tapston.com>
2022-08-05 16:04:18 +0300

    сreated all the necessary components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Header from '../src/components/header/Header';
+import Nav from '../src/components/nav/Nav';
+import About from '../src/components/about/About';
+import Experience from '../src/components/experience/Experience';
+import Services from '../src/components/services/Services';
+import Testimonials from '../src/components/testimonials/Testimonials';
+import Contact from '../src/components/contact/Contact';
+import Footer from '../src/components/footer/Footer';
+import Portfolio from '../src/components/portfolio/Portfolio';
 
 export const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
   )
 }
