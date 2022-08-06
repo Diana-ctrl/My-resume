ef18d67124644d22dbfcbb80fec23c343b194d31
Diana Lyah <diana.lyah.96@mail.ru>
2022-08-06 12:30:55 +0300

    work with layout Header

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,8 +5,8 @@ import './header.css';
 const HeaderSocials = () => {
   return (
     <div className='header_socials'>
-      <a href='https://www.linkedin.com/in/diana-lyah-7332641a2/' target='_blank' rel="noreferrer" ><BsLinkedin /></a>
-      <a href='https://github.com/Diana-ctrl' target='_blank' rel="noreferrer"><BsGithub /></a>
+      <a href='https://www.linkedin.com/in/diana-lyah-7332641a2/' target='_blank' rel="noreferrer" ><BsLinkedin size={'2rem'} /></a>
+      <a href='https://github.com/Diana-ctrl' target='_blank' rel="noreferrer"><BsGithub size={'2rem'} /></a>
     </div>
   )
 }
