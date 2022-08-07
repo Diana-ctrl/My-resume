91bdb9330f5a9293eb6f0d6e261f43562c9149a9
Diana Lyah <diana.lyah.96@mail.ru>
2022-08-07 12:56:18 +0300

    working with navigation

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,7 @@ import './about.css';
 
 const About = () => {
   return (
-    <div>A</div>
+    <section id='about'>A</section>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './nav.css';
+import { ImHome } from 'react-icons/im';
+import { FaUserSecret } from 'react-icons/fa';
+import { MdWorkOutline } from 'react-icons/md';
+import { RiServiceLine } from 'react-icons/ri';
+import { MdOutlineContactMail } from 'react-icons/md';
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#'><ImHome size={'2rem'} /></a>
+      <a href='#about'><FaUserSecret size={'2rem'} /></a>
+      <a href='#experience'><MdWorkOutline size={'2rem'} /></a>
+      <a href='#services'><RiServiceLine size={'2rem'} /></a>
+      <a href='#contact'><MdOutlineContactMail size={'2rem'} /></a>
+    </nav >
   )
 }
 
