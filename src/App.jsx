b21694809d2e097a371f5fd5062f028faebe1691
Diana Lyah <d.lyah@severex.io>
2024-02-19 14:26:35 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import Header from '../src/components/header/Header';
-import Nav from '../src/components/nav/Nav';
-import About from '../src/components/about/About';
-import Experience from '../src/components/experience/Experience';
-import Services from '../src/components/services/Services';
-import Testimonials from '../src/components/testimonials/Testimonials';
-import Contact from '../src/components/contact/Contact';
-import Footer from '../src/components/footer/Footer';
-import Portfolio from '../src/components/portfolio/Portfolio';
+import React from "react";
+import Header from "../src/components/header/Header";
+import Nav from "../src/components/nav/Nav";
+import About from "../src/components/about/About";
+import Experience from "../src/components/experience/Experience";
+import Services from "../src/components/services/Services";
+import Testimonials from "../src/components/testimonials/Testimonials";
+import Contact from "../src/components/contact/Contact";
+import Footer from "../src/components/footer/Footer";
+import Portfolio from "../src/components/portfolio/Portfolio";
 
 export const App = () => {
   return (
@@ -22,5 +22,5 @@ export const App = () => {
       <Contact />
       <Footer />
     </>
-  )
-}
+  );
+};
